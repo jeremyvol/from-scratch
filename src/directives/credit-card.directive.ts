925ae5299c565d3e0258fd9802001d5686d3d99e
Jeremy Vollant <j.vollant@gmail.com>
2023-10-11 13:25:59 +0200

    CreditCardDirective declarative rewrite

--- src/directives/credit-card.directive.ts
+++ src/directives/credit-card.directive.ts
@@ -2,22 +2,22 @@ import { Formatter } from "../services/formatter";
 import { CreditCardVerifier } from "../services/credit-card-verifier";
 import { Directive } from '../decorators/directive';
 
+import { HostBinding } from '../decorators/host-binding';
+import { HostListener } from '../decorators/host-listener';
+
 @Directive({ selector: '[credit-card]' })
 export class CreditCardDirective {
     constructor(
         private verifier: CreditCardVerifier,
         public element: HTMLElement,
         private formatter: Formatter,
-    ) { }
+    ) {}
 
+    @HostBinding('style.borderColor')
+    borderColor = 'blue';
+
+    @HostListener("input", ['event.target'])
     formatCreditCardNumber(element: HTMLInputElement) {
         element.value = this.formatter.formatNumber(element.value, 16, 4);
-    }
-
-    init() {
-        this.element.style.borderColor = "blue";
-        this.element.addEventListener('input', event => {
-            this.formatCreditCardNumber(event.target as HTMLInputElement);
-        });
     }
 }
