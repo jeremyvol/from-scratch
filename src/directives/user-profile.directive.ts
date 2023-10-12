594be068a5f86d4b76379cfc9bcf25f03943cdd4
Jeremy Vollant <j.vollant@gmail.com>
2023-10-12 20:50:21 +0200

    eventListeners bound to DOM

--- src/directives/user-profile.directive.ts
+++ src/directives/user-profile.directive.ts
@@ -15,25 +15,87 @@ export class UserProfileDirective {
     @Input('job')
     job: string;
 
+    template = `
+        <h3 (click)="onClickH3">
+        {{ firstName }} {{ lastName }}
+        </h3>
+        <strong>Poste : </strong>{{ job }}
+        <button (click)="onClickButton" (dblclick)="onDblClickButton">Changer le prenom</button>
+        `;
+
+    onClickH3() {
+        console.log('Click sur le h3');
+    }
+
+    onDblClickButton() {
+        this.firstName = "Marion";
+    }
+
+    onClickButton() {
+        this.firstName = "Roger";
+    }
+
 
     constructor(public element: HTMLElement) {}
 
     init() {
         this.render();
-
-        this.element.querySelector('button').addEventListener(
-            'click', () => {
-                this.firstName = "Roger";
-                this.render();
-            }
-        );
     }
 
     render() {
-        this.element.innerHTML = `
-        <h3>${this.firstName} ${this.lastName}</h3>
-        <strong>Poste : </strong>${this.job}
-        <button>Changer le prenom</button>
-        `;
+        let renderedTemplate = this.template;
+
+        // gestion des interpolations
+        this.template.match(/{{.*?}}/g).forEach(interpolation => {
+            const propName = interpolation.replace(/{{|}}/g, '').trim();
+            renderedTemplate = renderedTemplate.replace(interpolation, this[propName]);
+        });
+
+        const eventsToBind: {
+            elementId: string;
+            eventName: string;
+            methodName: string;
+        }[] = [];
+
+        this.template
+            .match(/<.*?\(.*?\)=\".*?\".*?>/g)
+            .forEach(baliseOuvrante => {
+                const randomId = 'event-listener-' + Math.ceil(Math.random() * 1000);
+
+                baliseOuvrante
+                    .match(/\(.*?\)=\".*?\"/g)
+                    .forEach(event => {
+                        const eventName = event
+                            .match(/\(.*?\)/g)[0]
+                            .replace(/\(|\)/g, '');
+                        const methodName = event
+                            .match(/\".*?\"/g)[0]
+                            .replace(/\"/g, '');
+
+                        eventsToBind.push({
+                            elementId: randomId,
+                            eventName,
+                            methodName
+                        });
+                    });
+                const finalBaliseOuvrante = baliseOuvrante
+                    .replace(/\(.*?\)=\".*?\"/g, '')
+                    .replace(/>/g, `id="${randomId}">`);
+
+                renderedTemplate = renderedTemplate.replace(
+                    baliseOuvrante,
+                    finalBaliseOuvrante
+                );
+            });
+        this.element.innerHTML = renderedTemplate;
+
+        eventsToBind.forEach(eventToBind => {
+            this.element
+                .querySelector(`#${eventToBind.elementId}`)
+                .addEventListener(eventToBind.eventName, () => {
+                    this[eventToBind.methodName]();
+                    this.render();
+                });
+        });
     }
 }
